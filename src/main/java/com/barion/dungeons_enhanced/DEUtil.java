9a8fa4bd3563ba529b0e452d82bcd7ff643d46fd
Barion <kothe@botos.de>
2021-12-21 10:14:51 +0100

    Implemented Processor Registry

--- src/main/java/com/barion/dungeons_enhanced/DEUtil.java
+++ src/main/java/com/barion/dungeons_enhanced/DEUtil.java
@@ -1,9 +1,25 @@
 package com.barion.dungeons_enhanced;
 
+import com.legacy.structure_gel.api.registry.RegistryHelper;
+import com.legacy.structure_gel.api.structure.processor.RandomBlockSwapProcessor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 
 public class DEUtil{
     public static ResourceLocation createRegistryName(String key){ return new ResourceLocation(DungeonsEnhanced.Mod_ID, key);}
     public static BlockPos Offset(int x, int y, int z){return new BlockPos(x, y, z);}
+
+    public class Processors {
+        public static final StructureProcessorList AirToCobweb = register("air_to_cobweb", new RandomBlockSwapProcessor(Blocks.AIR, Blocks.COBWEB));
+
+        private static StructureProcessorList register(String key, StructureProcessor processor){
+            return RegistryHelper.registerProcessor(createRegistryName(key), processor);
+        }
+        private static StructureProcessorList register(String key, StructureProcessorList processorList){
+            return RegistryHelper.registerProcessor(createRegistryName(key), processorList);
+        }
+    }
 }
