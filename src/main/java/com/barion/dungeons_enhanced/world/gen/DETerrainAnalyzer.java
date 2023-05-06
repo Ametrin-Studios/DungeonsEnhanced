7ffab580e982c8b922a64d7c3f514ce94ffe2bfd
Barion <kothe@botos.de>
2023-05-06 15:29:19 +0200

    updated forge

--- src/main/java/com/barion/dungeons_enhanced/world/gen/DETerrainAnalyzer.java
+++ src/main/java/com/barion/dungeons_enhanced/world/gen/DETerrainAnalyzer.java
@@ -22,7 +22,6 @@ public class DETerrainAnalyzer {
      */
     public static Pair<Float, Boolean> isFlatEnough(BlockPos pos, Vec3i size, int padding, int threshold, ChunkGenerator generator, LevelHeightAccessor heightAccessor, RandomState randomState){
         if(generator instanceof FlatLevelSource) {return Pair.of((float)generator.getBaseHeight(pos.getX(), pos.getZ(), Heightmap.Types.OCEAN_FLOOR_WG, heightAccessor, randomState), true);}
-//        if(!(heightAccessor instanceof ServerLevel level)) {return Pair.of(0f, false);}
 
         int x1 = pos.getX()+padding;
         int x2 = pos.getX()+size.getX()-padding;
@@ -31,13 +30,9 @@ public class DETerrainAnalyzer {
 
 
         int height1 = generator.getBaseHeight(x1, z1, Heightmap.Types.OCEAN_FLOOR_WG, heightAccessor, randomState);
-//        level.setBlockAndUpdate(new BlockPos(x1, height1, z1), Blocks.RED_WOOL.defaultBlockState());
         int height2 = generator.getBaseHeight(x2, z1, Heightmap.Types.OCEAN_FLOOR_WG, heightAccessor, randomState);
-//        level.setBlockAndUpdate(new BlockPos(x2, height2, z1), Blocks.RED_WOOL.defaultBlockState());
         int height3 = generator.getBaseHeight(x2, z2, Heightmap.Types.OCEAN_FLOOR_WG, heightAccessor, randomState);
-//        level.setBlockAndUpdate(new BlockPos(x2, height3, z2), Blocks.RED_WOOL.defaultBlockState());
         int height4 = generator.getBaseHeight(x1, z2, Heightmap.Types.OCEAN_FLOOR_WG, heightAccessor, randomState);
-//        level.setBlockAndUpdate(new BlockPos(x1, height4, z2), Blocks.RED_WOOL.defaultBlockState());
 
         float averageHeight = (height1+height2+height3+height4)/4f;
         float averageHeightDifference = (Math.abs(averageHeight-height1)+Math.abs(averageHeight-height2)+Math.abs(averageHeight-height3)+Math.abs(averageHeight-height4))/4f;
