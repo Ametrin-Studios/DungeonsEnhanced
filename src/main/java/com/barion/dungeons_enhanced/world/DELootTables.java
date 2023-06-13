9c01ff201f1244e83ee87d93b8f6c814bccca09e
Barion <kothe@botos.de>
2023-06-13 08:54:18 +0200

    refactor

--- src/main/java/com/barion/dungeons_enhanced/world/DELootTables.java
+++ src/main/java/com/barion/dungeons_enhanced/world/DELootTables.java
@@ -3,11 +3,11 @@ package com.barion.dungeons_enhanced.world;
 import com.barion.dungeons_enhanced.DEUtil;
 import net.minecraft.resources.ResourceLocation;
 
-public class DELootTables {
-    public static final ResourceLocation SUNKEN_SHRINE = chest("sunken_shrine");
-    public static final ResourceLocation MINERS_HOUSE = chest("miners_house");
+public interface DELootTables {
+    ResourceLocation SUNKEN_SHRINE = chest("sunken_shrine");
+    ResourceLocation MINERS_HOUSE = chest("miners_house");
 
-    public interface PILLAGER_CAMP{
+    interface PILLAGER_CAMP{
         ResourceLocation GENERAL = chest("pillager_camp/general");
         ResourceLocation KITCHEN = chest("pillager_camp/kitchen");
     }
